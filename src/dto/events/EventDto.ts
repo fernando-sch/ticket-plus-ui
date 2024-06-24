27edaa9cb4b9bf10182607881186aa74d896112f
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-24 17:06:50 -0300

    chore: add event dto

---
+++ src/dto/events/EventDto.ts
@@ -0,0 +1,53 @@
+type RemoteEvent = {
+  id: string;
+  name: string;
+  organization: string;
+  date: string;
+  price: number;
+  rating: string;
+  image_url: string;
+  location: string;
+};
+
+export class EventDto {
+  constructor(
+    public id: string,
+    public name: string,
+    public organization: string,
+    public date: string,
+    public price: number,
+    public rating: string,
+    public imageUrl: string,
+    public location: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.organization = organization;
+    this.date = date;
+    this.price = price;
+    this.rating = rating;
+    this.imageUrl = imageUrl;
+    this.location = location;
+  }
+}
+
+export const buildEventDto = ({
+  id,
+  name,
+  organization,
+  date,
+  price,
+  rating,
+  image_url,
+  location,
+}: RemoteEvent) =>
+  new EventDto(
+    id,
+    name,
+    organization,
+    date,
+    price,
+    rating,
+    image_url,
+    location
+  );
