80fe6ffe8cc67704db3c4d66c19fce0c8a4f78b2
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-25 17:57:57 -0300

    chore: update use fetch events hook to be used on client and server calls

--- src/hooks/useFetchEvents/useFetchEvents.ts
+++ src/hooks/useFetchEvents/useFetchEvents.ts
@@ -1,16 +1,12 @@
 import { axiosInstance } from "@/config/AxiosInstance";
 import { EventDto, buildEventDto } from "@/dto/events/EventDto";
-import { useQuery } from "@tanstack/react-query";
 
 export const useFetchEvents = () => {
-  const fetchTasks = async () => {
+  const fetchTasks = async (): Promise<EventDto[]> => {
     return await axiosInstance
       .get("/events")
       .then((response) => response.data.events.map(buildEventDto));
   };
 
-  return useQuery<EventDto[]>({
-    queryKey: ["fetchEvents"],
-    queryFn: fetchTasks,
-  });
+  return { queryKey: ["fetchEvents"], queryFn: fetchTasks };
 };
