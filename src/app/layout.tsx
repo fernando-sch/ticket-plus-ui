ade0cda058b174c42912066d5c10694bda8a7e79
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-26 16:34:27 -0300

    chore: update layout to use navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "./providers/ReactQueryProvider";
+import { Navbar } from "@/components/NavBar/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,10 @@ const RootLayout = ({ children }: RootLayoutProps) => {
         className={`${inter.className} flex flex-col min-h-screen items-center bg-primary text-default`}
       >
         <ReactQueryProvider>
-          <div className="p-4 md:p-10 w-full max-w-[1256px]">{children}</div>
+          <div className="p-4 md:p-10 w-full max-w-[1256px]">
+            <Navbar />
+            {children}
+          </div>
         </ReactQueryProvider>
       </body>
     </html>
